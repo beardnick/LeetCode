7a9be16d37a75f17e89222943327db261c9f090b
qianz <1685437606@qq.com>
2019-06-04 19:05:26 +0800

    尝试最长有效括号，还是不能瞎试

--- src/practice/longestValidParentheses.py
+++ src/practice/longestValidParentheses.py
@@ -14,38 +14,38 @@ def longestValidParentheses(s: str) -> int:
                 #     longest = max(longest,cnt)  - len(stack)
                 #     stack = []
                 #     cnt = 0
-                if len(stack) == 0 or (i == ')' and stack[-1] != '(' ):
-                    if len(stack) != 0:
-                        longest = max(longest, thiscnt - len(stack))
-                    else:
-                        longest = max(thiscnt + lastcnt, longest)
-                    print("len of stack :" , len(stack))
-                    print("thiscnt :" , thiscnt)
-                    print("lastcnt :" , lastcnt)
-                    print("longest :" , longest)
-                    longest = max(longest,thiscnt)
-                    lastcnt = thiscnt - len(stack)
-                    stack = []
-                    thiscnt = 0
+                if len(stack) != 0:
+                    longest = max(longest, thiscnt - len(stack))
+                else:
+                    longest = max(thiscnt + lastcnt, longest)
+                # print("len of stack :" , len(stack))
+                # print("thiscnt :" , thiscnt)
+                # print("lastcnt :" , lastcnt)
+                # print("longest :" , longest)
+                lastcnt = thiscnt - len(stack)
+                stack = []
+                thiscnt = 0
             else:
                 stack.append(i)
                 thiscnt +=1
-        print("len of stack :" , len(stack))
-        print("thiscnt :" , thiscnt)
-        print("lastcnt :" , lastcnt)
-        print("longest :" , longest)
+        # print("len of stack :" , len(stack))
+        # print("thiscnt :" , thiscnt)
+        # print("lastcnt :" , lastcnt)
+        # print("longest :" , longest)
         if len(stack) != 0:
             longest = max(longest, thiscnt - len(stack))
         else:
             longest = max(thiscnt + lastcnt, longest)
         return longest
                  
-# print(longestValidParentheses("(()"))
-# print(longestValidParentheses("(()()"))
-# print(longestValidParentheses(")()())"))
-# print(longestValidParentheses("())))"))
-# print(longestValidParentheses("()(()"))
+print(longestValidParentheses("(()"))
+print(longestValidParentheses("(()()"))
+print(longestValidParentheses(")()())"))
+print(longestValidParentheses("())))"))
+print(longestValidParentheses("()(()"))
 print(longestValidParentheses(")()())()()("))
+print(longestValidParentheses(")())))))()"))
+print(longestValidParentheses("(()(((()"))
 
 # brackets  = input()
 # print(longestValidParentheses(brackets))
